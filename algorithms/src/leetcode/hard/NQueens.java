11694297245d2fd7a7ef0de8f73fc5b3b2d2ed0a
baibhav <baibhav@bob-Nitro-AN515-54>
2022-03-25 07:00:54 -0400

    Nqueens solution added

---
+++ algorithms/src/leetcode/hard/NQueens.java
@@ -0,0 +1,54 @@
+package leetcode.hard;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class NQueens {
+	
+	Set<Integer> cols, left2rightDiag, right2leftDiag;
+
+	public List<List<String>> solveNQueens(int n) {
+
+		cols = new HashSet<>();
+		left2rightDiag = new HashSet<>();
+		right2leftDiag = new HashSet<>();
+
+		List<List<String>> ans = new ArrayList<>();
+		helper(n, ans, new ArrayList<>());
+		return ans;
+	}
+
+	void helper(int n, List<List<String>> ans, List<String> res) {
+
+		if (res.size() == n)
+			ans.add(new ArrayList<>(res));
+
+		for (int col = 0; col < n; col++) {
+
+			if (cols.contains(col) ||
+				left2rightDiag.contains(col - res.size()) ||
+				right2leftDiag.contains(col + res.size())
+			)
+				continue;
+
+			cols.add(col);
+			left2rightDiag.add(col - res.size());
+			right2leftDiag.add(col + res.size());
+
+			StringBuilder sb = new StringBuilder();
+			sb.append(".".repeat(col));
+			sb.append("Q");
+			sb.append(".".repeat(n - 1 - col));
+			res.add(sb.toString());
+
+			helper(n, ans, res);
+
+			res.remove(res.size() - 1);
+			cols.remove(col);
+			left2rightDiag.remove(col - res.size());
+			right2leftDiag.remove(col + res.size());
+		}
+	}
+}
